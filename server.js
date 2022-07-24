a7217706866244acac84b8c959b7b73f7d30043d
Sergius92739 <sergem279@gmail.com>
2022-07-24 15:45:03 +0300

    fixed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const Koa = require('koa');
 const koaBody = require('koa-body');
-const cors = require('@koa/cors');
+const cors = require('koa2-cors');
 const router = require('./routes');
 
 const app = new Koa();
